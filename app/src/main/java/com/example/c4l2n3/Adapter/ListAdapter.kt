356ab130d2c07cfcd4ceecf6464e98a849365040
Pikosha2101 <Puka4yBTanke@gmail.com>
2023-10-09 03:23:59 +0300

    готово

--- app/src/main/java/com/example/c4l2n3/Adapter/ListAdapter.kt
+++ app/src/main/java/com/example/c4l2n3/Adapter/ListAdapter.kt
@@ -5,24 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.ImageView
-import android.widget.TextView
 import com.example.c4l2n3.R
+import com.example.c4l2n3.databinding.ListViewItemBinding
 import com.example.c4l2n3.models.ItemModel
 
 class ListAdapter (context: Context, private val arrayList: ArrayList<ItemModel>) :
     ArrayAdapter<ItemModel>(context, R.layout.list_view_item, arrayList) {
 
+    private lateinit var binding : ListViewItemBinding
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val inflater = LayoutInflater.from(parent.context)
-        val view : View = inflater.inflate(R.layout.list_view_item, parent, false)
+        binding = ListViewItemBinding.inflate(inflater, parent, false)
 
-        val imageView : ImageView = view.findViewById(R.id.iconImageView)
-        val dishName : TextView = view.findViewById(R.id.nameTextView)
+        binding.iconImageView.setImageResource(arrayList[position].image)
+        binding.nameTextView.text = arrayList[position].name
 
-        imageView.setImageResource(arrayList[position].image)
-        dishName.text = arrayList[position].name
-
-        return view
+        return binding.root
     }
 }
